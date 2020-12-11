faf777739a1c6ca0c7e48f26692733289abec550
utkarshagod <70024776+utkarshagod@users.noreply.github.com>
2020-12-11 20:13:38 +0530

    Add files via upload
    react

---
+++ App.js
@@ -0,0 +1,104 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+      <div className="row">
+       
+       <center> 
+       <header>
+       <img src="logo.png" width="500px" /> 
+       <h3>C.R.I PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time</h3> 
+       <div className="imgd">
+      
+       <p>C.R.I's energy efficient product are well recognized by various Government Institution ,as trustworth product <br />for various project across the globe to save energy.</p>
+       </div>
+       
+       
+       
+       
+      
+       <div className="imgd3">
+       <p>C.R.I is the highest constributor in the country for the project of EESL(Energy Efficiency Services Limited)to replace the</p>
+       <p> old inefficient pumps with 5 star rated energy efficient smart pumps with IoT enabled control panel.</p>
+       </div>
+      </header>  
+       <img src="1.png" width="276px" />  
+
+       <img alt="" src="2.png" width="650px" height="400px"  /> 
+       <div className="par">
+       <p>Government of India has awarded the <b>"National Energy Conservation Award 2018".</b>Mr.G.Selvaraj,joint  Managing Director</p>
+       <p>of C.R.I Group received the award from smt.Sumitra Mahajan,Speaker of Lok Sabha & Shri.Raj Kumar Singh,Honorable</p>
+       <p>Minister of State </p>
+       </div>
+       <div className="cap">
+       <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
+       </div>
+        <img alt="" src="3.png" width="990px" />
+        
+        
+        <header>
+          
+        <div className="hr"> 
+            
+            <hr />
+         </div>
+			<h4><span>C.R.I FLUID PRODUCTS CATER TO DIVERSE SEGMENTS</span></h4>
+      <h5>CHEMICALS & PROCESS|POWER|WATER & WASTE WATER|OILS & GAS|PHARMA|SUGARS & DISTILLERIES|PAPER & PULP|MARINE & DEFENCE|METAL AND MINING|FOOD & BEVERAGE|
+        PETROCHEMICAL & REFINERIES|SOLAR|BUILDING|HVAC|FIRE FIGTING|AGRICULTURE & RESIDENTIAL
+      </h5>
+		  </header>
+      
+     
+      <footer className="footer-distributed">
+
+			<div className="footer-left">
+          <p class="footer-company-name">Toll free 1800 200 1234</p>
+
+			</div>
+
+
+			<div className="footer-center">
+      <div>
+					<i class="fa fa-map-marker"></i>
+					  <p><span></span>
+						www.facebook.com/cripumps</p>
+				</div>
+				
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p></p>
+				</div>
+				
+			</div>
+     
+     
+			<div class="footer-right">
+      <p class="footer-company-about">
+					<span>www.crigroups.com</span>
+					</p>
+				<div class="footer-icons">
+					
+				</div>
+				
+				</div>
+      
+		</footer>
+
+
+
+
+    </center>
+
+
+
+
+          </div>
+     
+   
+    
+  );
+}
+
+export default App;
